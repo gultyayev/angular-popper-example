2e216ee6d47476f42afddf218e24f32c1502b41d
Сергей <gultyayev.sergey@gmail.com>
2019-08-03 00:06:00 +0300

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopperDirective } from './popper.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopperDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/popper.directive.ts
@@ -0,0 +1,83 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2
+} from "@angular/core";
+import Popper, { PopperOptions } from "popper.js";
+import { fromEvent, merge, Subject } from "rxjs";
+import { filter, pluck, takeUntil } from "rxjs/operators";
+
+@Directive({
+  selector: "[appPopper]"
+})
+export class PopperDirective implements OnInit, OnDestroy {
+  // The hint to display
+  @Input() target: HTMLElement;
+  // Its positioning (check docs for available options)
+  @Input() placement?: string;
+  // Optional hint target if you desire using other element than specified one
+  @Input() appPopper?: HTMLElement;
+  // The popper instance
+  private popper: Popper;
+  private readonly defaultConfig: PopperOptions = {
+    placement: "top",
+    removeOnDestroy: true,
+    modifiers: {
+      arrow: {
+        element: ".popper__arrow"
+      }
+    },
+    eventsEnabled: false
+  };
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(
+    private readonly el: ElementRef,
+    private readonly renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    // An element to position the hint relative to
+    const reference = this.appPopper ? this.appPopper : this.el.nativeElement;
+
+    this.popper = new Popper(reference, this.target, this.defaultConfig);
+
+    this.renderer.setStyle(this.target, "display", "none");
+
+    merge(
+      fromEvent(reference, "mouseenter"),
+      fromEvent(reference, "mouseleave")
+    )
+      .pipe(
+        filter(() => this.popper != null),
+        pluck("type"),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((e: any) => this.mouseHoverHandler(e));
+  }
+
+  ngOnDestroy(): void {
+    if (!this.popper) {
+      return;
+    }
+
+    this.popper.destroy();
+
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private mouseHoverHandler(e: string): void {
+    if (e === "mouseenter") {
+      this.renderer.removeStyle(this.target, "display");
+      this.popper.enableEventListeners();
+      this.popper.scheduleUpdate();
+    } else {
+      this.renderer.setStyle(this.target, "display", "none");
+      this.popper.disableEventListeners();
+    }
+  }
+}
